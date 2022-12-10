3d289e4b7a47d96d902f4af0ef580fd3c1acfb6a
Diogo Loiola <diogomouraloiola@gmail.com>
2022-12-10 14:46:18 -0300

    add content-type application/json

--- lib/http_request.rb
+++ lib/http_request.rb
@@ -1,6 +1,6 @@
 require 'typhoeus'
 require 'json'
-
+require 'byebug'
 class HttpRequest # rubocop:disable Style/Documentation
   def initialize(webhook_url)
     @webhook_url = webhook_url
@@ -15,18 +15,20 @@ class HttpRequest # rubocop:disable Style/Documentation
   def request(body: {}, verbose: false) # rubocop:disable Metrics/MethodLength
     body = body.to_h
     body = body.delete_if { |_k, v| v.nil? || v.to_s.empty? }
+    body = body.to_json
 
-    request = Typhoeus::Request.new(
+    response = Typhoeus::Request.new(
       @webhook_url,
       method: :post,
       body:,
+      headers: { 'Content-Type': 'application/json' },
       verbose:
     ).run
 
-    if request.success?
-      JSON.parse(request.body) unless request.body.empty?
+    if response.success?
+      JSON.parse(response.body) unless response.body.empty?
     else
-      error_api = JSON.parse(request.body)
+      error_api = JSON.parse(response.body)
       { error: error_api }
     end
   end
